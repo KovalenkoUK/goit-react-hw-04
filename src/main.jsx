4e201e74c3b8d9c5ec2f1c4c3fc018547c6daa64
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-01-31 22:47:25 +0200

    js

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "modern-normalize";
-import './style.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <App />
+    </>
+  </StrictMode>
+);
